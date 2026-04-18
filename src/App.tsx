7d9610d6d6f220385158c8768dcde55ff648275a
githubscientist <sathish.datascientist@gmail.com>
2026-04-18 11:46:42 +0530

    class components in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello World!</h1>
+      </>
+    )
+  }
 }
 
 export default App;
